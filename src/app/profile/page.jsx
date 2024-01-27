495fb0fba2d5d4ed43a298a41ad7b6f6e0661406
adarsh1278 <120383844+adarsh1278@users.noreply.github.com>
2024-01-27 18:31:06 +0530

    error handled

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,44 +1,39 @@
 "use client";
-import Image from "next/image";
-import { useState } from "react";
-export default function profile(){
-  
-let img;
-const [render , setRender] = useState("");
-const animation =    <div className="flex items-center justify-center h-screen bg-blur">
-<div className="bg-white bg-opacity-50 p-8 rounded-lg shadow-lg">
-  <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid"></div>
-</div>
-</div>
- async function handle(){
-  setRender(animation)
-  const response = await fetch('/api/user/profile', {
-    method: 'GET'
+
+import React, { useState } from "react";
 
 
+export default function Profile() {
+  let img;
+  const [render, setRender] = useState("");
 
-}
-  )
-  console.log("response below")
-  const data =  await response.json();
-  console.log("data is given below")
-  console.log( data.data.avtar)
- img = data.data.avtar;
- setRender(<img src={img} alt="no image"/>)
- 
-}
+  const animation = (
+    <div className="flex items-center justify-center h-screen bg-blur">
+      <div className="bg-white bg-opacity-50 p-8 rounded-lg shadow-lg">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid"></div>
+      </div>
+    </div>
+  );
 
-return(
-  <>
-  <div className=" flex justify-center  items-center bg-slate-300 text-2xl w-screen h-screen">
-    <button onClick={handle}  className=" bg-slate-600 text-white p-5 m-8">Click me</button>
-    
-    {render}
-  </div>
- 
+  async function handle() {
+    setRender(animation);
+    const response = await fetch('/api/user/profile', {
+      method: 'GET'
+    });
 
-  
-  </>
-)
+    const data = await response.json();
+    img = data.data.avtar;
+    setRender(<img src={img} alt="no image" />);
+  }
 
+  return (
+    <>
+      <div className="flex justify-center items-center bg-slate-300 text-2xl w-screen h-screen">
+        <button onClick={handle} className="bg-slate-600 text-white p-5 m-8">
+          Click me
+        </button>
+        {render}
+      </div>
+    </>
+  );
 }
