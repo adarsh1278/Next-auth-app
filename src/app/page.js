9c93b5d219b941527a1682ecaa52a541e21990e5
adarsh1278 <120383844+adarsh1278@users.noreply.github.com>
2024-01-27 18:26:20 +0530

    loading animation added in profile

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,53 @@
-export default function Home(){
-  return(
-    <div>
-      hOme page
+import React from 'react';
+
+const WaveComponent = () => {
+  const waveStyle = {
+    textAlign: 'center',
+  };
+
+  const waveItemStyle = {
+    width: '5px',
+    height: '100px',
+    background: 'linear-gradient(45deg, cyan, #fff)',
+    margin: '10px',
+    animation: 'wave 1s linear infinite',
+    borderRadius: '20px',
+  };
+
+  return (
+    <div style={waveStyle}>
+      {[...Array(10)].map((_, index) => (
+        <div
+          key={index}
+          style={{
+            ...waveItemStyle,
+            animationDelay: `${index * 0.1}s`,
+          }}
+        ></div>
+      ))}
+      <style>
+        {`
+          body {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+          }
+
+          @keyframes wave {
+            0% {
+              transform: scale(0);
+            }
+            50% {
+              transform: scale(1);
+            }
+            100% {
+              transform: scale(0);
+            }
+          }
+        `}
+      </style>
     </div>
-  )
-}
+  );
+};
+
+export default WaveComponent;
--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,7 +1,17 @@
 "use client";
+import Image from "next/image";
+import { useState } from "react";
 export default function profile(){
-
-const handle = async() =>{
+  
+let img;
+const [render , setRender] = useState("");
+const animation =    <div className="flex items-center justify-center h-screen bg-blur">
+<div className="bg-white bg-opacity-50 p-8 rounded-lg shadow-lg">
+  <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid"></div>
+</div>
+</div>
+ async function handle(){
+  setRender(animation)
   const response = await fetch('/api/user/profile', {
     method: 'GET'
 
@@ -12,14 +22,21 @@ const handle = async() =>{
   console.log("response below")
   const data =  await response.json();
   console.log("data is given below")
-  console.log( data)
-
+  console.log( data.data.avtar)
+ img = data.data.avtar;
+ setRender(<img src={img} alt="no image"/>)
+ 
 }
+
 return(
   <>
-  <div>
-    <button onClick={handle} className=" bg-slate-600 text-white p-5 m-8">Click me</button>
+  <div className=" flex justify-center  items-center bg-slate-300 text-2xl w-screen h-screen">
+    <button onClick={handle}  className=" bg-slate-600 text-white p-5 m-8">Click me</button>
+    
+    {render}
   </div>
+ 
+
   
   </>
 )
